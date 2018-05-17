bb2931357ab692dd3dbe1c0206301f5a6f4c2f55
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-05-17 21:17:22 +0800

    Main javascript library move to "dist" directory  - move "cellbis.js" to "dist" directory  - add file "cellbis-pagination.js" in "dist" directory\    for Pagination feature.

---
+++ dist/cellbis-pagination.js
@@ -0,0 +1,9 @@
+/*!
+ * Filename : cellbis-pagination.min.js
+ * Author: Achmad Yusri Afandi (yusrideb@cpan.org)
+ * Date Create : 4/17/18 7:32 AM
+ *
+ * Copyright Achmad Yusri Afandi (yusrideb@cpan.org)
+ * Released under the Artistic License 2.0
+ *
+ */
