8390215a7824a2b29c168b758576d40b892e52c5
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-06-29 06:20:12 +0800

    Prepare NPM : Update package.json to add license information

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
  * Filename : index.js
  * Author: Achmad Yusri Afandi (yusrideb@cpan.org)
  * Date Create : 6/29/18 5:08 AM
+ *
+ * Copyright Achmad Yusri Afandi (yusrideb@cpan.org)
+ * Released under the Artistic License 2.0
+ *
  */
 
 module.exports = require('./src/core.js');
