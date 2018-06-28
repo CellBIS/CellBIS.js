2e6958b5946374d94704c8a838304b7d235926bb
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-06-29 05:12:39 +0800

    Prepare NPM : Add licence information in comment header file.

--- src/var/isBrowser.js
+++ src/var/isBrowser.js
@@ -2,6 +2,10 @@
  * Filename : isBrowser.js
  * Author: Achmad Yusri Afandi (yusrideb@cpan.org)
  * Date Create : 6/29/18 5:07 AM
+ *
+ * Copyright Achmad Yusri Afandi (yusrideb@cpan.org)
+ * Released under the Artistic License 2.0
+ *
  */
 define(function() {
   
